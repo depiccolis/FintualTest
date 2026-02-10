3c7170cf7bff5831daf7a5eab3db0a3ec3c76d26
depiccolis <depiccolis@gmail.com>
2026-02-09 23:07:03 -0300

    Add comments, README.txt and verification for portfolio

---
+++ README.txt
@@ -0,0 +1,12 @@
+Este proceso calcula si frente a cambios en precios de acciones
+de un portafolio, se necesita comprar o vender acciones
+tal de mantener una distribución elegida. Por simpplicidad,
+se trabaja con un valor al azar para cada acción entre un
+rango de 1 y 100
+
+El proceso recibe cuatro inputs:
+
+stock_list: lista con acciones de interés
+portfolio: diccionario con porcentaje de interés para cada acción
+iterations: cantidad de iteraciones que se desea para el proceso
+initial_capital: capital inicial al comienzo del proceso
--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import random
 
+# Function that mimics the change in stock prices
 def get_stock_price():
     return random.randint(1, 100)
 
 # INPUT
-stock_list = ['APPL', 'META']
-portfolio = {'APPL': 40, 'META': 60}
+stock_list = ['APPL', 'META', 'GOOGL'] #
+portfolio = {'APPL': 10, 'GOOGL': 50, 'META': 40}
 iterations = 4
 initial_capital = 1000
 
-# PROCESS
+############# START PROCESS ################
+
+# Verify portfolio is properly created
+contador_percentage = 0
+for stock in portfolio:
+    contador_percentage += portfolio[stock]
+
+if contador_percentage != 100:
+    raise 'Portfolio does not add up to 100'
 
 d_prices = {}
 contador = 0
 
+# Get the initial stock prices
 for stock in stock_list:
     d_prices[stock] = {}
     d_prices[stock][0] = get_stock_price()
@@ -21,6 +31,7 @@ for stock in stock_list:
 
 print(f'Capital ({contador}): {initial_capital}')
 
+# Get te initial quantity of every stock
 d_quantity = {}
 
 for stock in stock_list:
@@ -28,7 +39,7 @@ for stock in stock_list:
     d_quantity[stock][0] = initial_capital*portfolio[stock]/100/d_prices[stock][0]
     print(f'{stock} stock quentity ({contador}): {d_quantity[stock][0]}')
 
-# Start process
+# Start the iterations
 
 while contador < iterations:
 
@@ -36,15 +47,19 @@ while contador < iterations:
 
     print(f'{contador}-----------------')
 
+    # Get the new prices per stock
     for stock in stock_list:
         d_prices[stock][contador] = get_stock_price()
         print(f'{stock} stock price ({contador}): {d_prices[stock][contador]}')
 
+    # Get the new capital
     new_caital = 0
     for stock in stock_list:
         new_caital += d_prices[stock][contador]*d_quantity[stock][contador-1]
 
     print(f'Capital ({contador}): {new_caital}')
+
+    # Get the new quantity of every stock
 
     for stock in stock_list:
         d_quantity[stock][contador] = new_caital*portfolio[stock]/100/d_prices[stock][contador]
@@ -56,11 +71,11 @@ while contador < iterations:
 
         if d_quantity[stock][contador] < d_quantity[stock][contador-1]:
             go_short = d_quantity[stock][contador-1]-d_quantity[stock][contador]
-            print(f'Vender {go_short} {stock}')
+            print(f'{stock} GO SHORT {go_short}')
         elif d_quantity[stock][contador] == d_quantity[stock][contador-1]:
-            print('Do nothing')
+            print(f'{stock} DO NOTHING')
         else:
             go_long = d_quantity[stock][contador] - d_quantity[stock][contador-1]
-            print(f'Comprar {go_long} {stock}')
+            print(f'{stock} GO LONG {go_long}')
 
 print('Finish!!')
